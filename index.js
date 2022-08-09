f118068636d014346ba2e13a5e2334ff118f782e
NarendraRaj19 <ramasamykanchanama.n@northeastern.edu>
2022-08-08 21:53:30 -0400

    Final Working Code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+let responseObject = {};
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,37 +25,37 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-app.get("/api/:timestamp", function(req, res){
-  var date = new Date(req.params.timestamp);
-
-  //console.log("Test: ",date.getUTCFullYear()," ",date.getUTCMonth());
-
-  var d2 = new Date(date.getUTCFullYear(),date.getUTCMonth(), date.getUTCDate()-1, date.getHours(), date.getUTCMinutes(), date.getUTCSeconds());
-  //console.log("Test2 : ",d2.toUTCString());
-
-
-  // let year = date.getFullYear(); // 2020
-  // let month = date.getMonth() + 1; // 4 (note zero index: Jan = 0, Dec = 11)
-  // let day = date.getDate() + 1; // 9
-  
-  // const gmtDateTime = new Date().toUTCString();
-  // console.log("UTC String: ",gmtDateTime);
-  
-  //let timestmp = date.getTime();
-  
-  
-  //console.log("URL Hit:", year ,":: ", month ," ", day," ",date.getUTCDay()," ",date.getUTCMonth());
-  //{"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
-  res.json({"unix":date.getTime(),"utc":d2.toUTCString()});
+app.get("/api", function(req, res){
+    console.log("Inside W/O parameter step");
+    var resDate = new Date();
+    res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
 });
 
-// app.get("/api/", function(req, res) {
-//   var resDate = new Date();
-//   res.json({ unix: resDate.valueOf(), utc: resDate.toUTCString() });
-// });
+//Route to handle if date/timestamp is provided
+app.get("/api/:timestamp", function(req, res){
 
-
+  let input = req.params.timestamp;
+  
+  if(input.includes(' ') || input.includes('-')){
+    /* Date String */
+      responseObject['unix'] = new Date(input).getTime()
+      responseObject['utc'] = new Date(input).toUTCString()
+  } else{
+    /* Timestamp */
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    res.json({error: 'Invalid Date'})
+  } else {
+    res.json(responseObject)
+  }
+  
+  
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
